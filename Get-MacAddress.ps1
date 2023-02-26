5f1eea4e2bd40d18c4ec19fc3feb79635c5228bd
Martin Sandgaard <msandgaard@gmail.com>
2023-02-26 20:18:21 +0100

    Get-MacAddress is done

--- Get-MacAddress.ps1
+++ Get-MacAddress.ps1
@@ -7,27 +7,20 @@ function Get-MacAddress {
 [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
 [string]$IPAddress)
     
-s
+
 try{
-    $result = Get-NetNeighbor -IPAddress $IPAddress
+    Get-NetNeighbor -IPAddress "$IPAddress" -ErrorAction Stop
     return $result.LinkLayerAddress
 }
 
 catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]{
     Get-NetIPAddress | % {
         if ($_.IPAddress -eq $IPAddress){
-            
-            
-            #IP Addresses on local Nic
-             % {Get-NetIPAddress -InterfaceIndex $_.InterfaceIndex | select IPv4Address}
-            
+           #Get InterfaceIndex of the NIC owning the mac address
+           $IfIndex = Get-NetIPAddress -InterfaceIndex $_.InterfaceIndex | Select-Object InterfaceIndex | sort -Unique
             #Mac Addresses on local NIC
-            $LocalMacAddresses = Get-NetAdapter | Select MacAddress, IfIndex
-            $LocalIPAddresses = $LocalMacAddresses | % { Get-NetIPAddress -InterfaceIndex $_.IfIndex | select IPv4Address | Sort-Object -Unique}
-            
-            
-
-            return $
+            $LocalMacAddress = Get-NetAdapter | ? { $_.ifIndex -eq $IfIndex.InterfaceIndex}
+           return $LocalMacAddress.MacAddress
         }
     }
     
